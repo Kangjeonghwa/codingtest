25061aa85b3b062c76ea3652f122aa267659c62a
Kangjeonghwa <102348981+Kangjeonghwa@users.noreply.github.com>
2022-04-23 23:12:16 +0900

    Add files via upload

---
+++ ch2/QuadrantN.java
@@ -0,0 +1,27 @@
+package ch2;
+
+import java.util.Scanner;
+
+public class QuadrantN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X, Y;
+
+        X=sc.nextInt();
+        Y=sc.nextInt();
+
+        if(X>0 && Y>0){
+            System.out.println("1");
+        }
+        else if(X<0 && Y>0){
+            System.out.println("2");
+        }
+        else if(X<0 && Y<0){
+            System.out.println("3");
+        }
+        else if(X>0 && Y<0){
+            System.out.println("4");
+        }
+        sc.close();
+    }
+}
